0f55bf55d0d28b4c064d4819528568819d1d1b1b
Jonas Hubert <jonas.hubert@student.howest.be>
2018-04-06 23:25:32 +0200

    getDocument informatie weergegeven op pagina indien reeds registered

--- truffle/src/js/app.js
+++ truffle/src/js/app.js
@@ -61,24 +61,34 @@ App = {
         //todo: uitzoeken welke, of het aanvaardbaar is. indien niet, anders oplossen.
         reader.onload = function(e) {
           //(bytes32 _hash, bytes32 _author,bytes32 _title,bytes32 _email,uint256 _dateWritten,uint256 _dateRegistered)
-          var hash = CryptoJS.SHA1(reader.result).toString();
+          var hash = web3.sha3(reader.result);
 
 
-          docAuthChecker.isDocumentRegistered.call(hash).then(
-            function(exists) {
-              if (exists) {
-                /*docAuthChecker.getDocument.call(hash).then (
-                  function(result) {
-                    var res = "Document already registered: " + "<br> Document Owner: " + result[0];
-                    $("#docinfo").html(res);*/
-                    $("#docinfo").html("Exists");
-                 // }
-               // )
+          docAuthChecker.getDocument.call(hash).then(
+            function(tuple) {
+              var isInitialized = tuple[5];
+              if (isInitialized) {
+                var author = web3.toAscii(tuple[0]);
+                var title = web3.toAscii(tuple[1]);
+                var email = web3.toAscii(tuple[2]);
+                var dateWritten = tuple[3];
+                var dateRegistered =  tuple[4];
+$('#docinfo').css('font-size','120%');
+$("#docinfo").append("<h3>This document was already registered! Here's some information about it:</h3>")
+                $("#docinfo").append('<p>Author: ' + author + '</p>')
+                $("#docinfo").append('<p>Title: ' + title + '</p>')
+                $("#docinfo").append('<p>Author\'s emailaddress: ' + email + '</p>')
+                $("#docinfo").append('<p>Written on: ' + dateWritten + '</p>')
+                $("#docinfo").append('<p>Registered on: ' + dateRegistered + '</p>')
+
               } else {
                 var date = $('#dateWrittenInput').datetimepicker('viewDate')
                 var dateFormatted = parseInt(date.toISOString().slice(0,10).replace(/-/g,""));
                 //register(bytes32 _hash, bytes32 _author,bytes32 _title,bytes32 _email,uint256 _dateWritten
-                var result = docAuthChecker.register(hash, $('#author').val(), $('#title').val(), $('#email').val(), dateFormatted, {from: account});
+                var author = web3.toHex($('#author').val());
+                var title = web3.toHex($('#title').val());
+                var email = web3.toHex($('#email').val());
+                var result = docAuthChecker.register(hash, author, title, email , dateFormatted, {from: account});
               }
             }
           )
@@ -94,7 +104,6 @@ App = {
   }
 
 };
-
 
 $(function() {
   $(window).on('load', function() {
