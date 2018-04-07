965d7d8d6d55837873026984648cc3636569bb78
Jonas Hubert <jonas.hubert@student.howest.be>
2018-04-07 13:01:42 +0200

    navigatie toegevoegd. check authenticity pagina toegevoegd. formattering + layout (nog niet afgewerkt)

--- truffle/src/js/app.js
+++ truffle/src/js/app.js
@@ -35,9 +35,83 @@ App = {
 
   bindEvents: function() {
     $(document).on('click', '#registerButton', App.register);
+    $(document).on('click', '#checkAuthenticityButton', App.checkAuthenticity);
+
   },
 
   register: function(event) {
+    event.preventDefault();
+
+    var docAuthChecker;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.DocAuth.deployed().then(function(instance) {
+        docAuthChecker = instance;
+
+
+        var fileInput = document.getElementById('document');
+        var file = fileInput.files[0];
+        var reader = new FileReader();
+
+        //note: gebruik van FileReader maakt het erg beperkt qua browser support
+        //todo: uitzoeken welke, of het aanvaardbaar is. indien niet, anders oplossen.
+        reader.onload = function(e) {
+          //(bytes32 _hash, bytes32 _author,bytes32 _title,bytes32 _email,uint256 _dateWritten,uint256 _dateRegistered)
+          var hash = web3.sha3(reader.result);
+
+
+          docAuthChecker.getDocument.call(hash).then(
+            function(tuple) {
+              $('.mainForm').hide();
+              $('#docinfo').show();
+
+              var isInitialized = tuple[5];
+              if (isInitialized) {
+                var author = web3.toAscii(tuple[0]);
+                var title = web3.toAscii(tuple[1]);
+                var email = web3.toAscii(tuple[2]);
+                var dateWrittenTemp = Date.parseExact(tuple[3].toString(),"yyyyMMdd");
+                var dateWritten = dateWrittenTemp.toString("dd-MM-yyyy");
+                var dateRegistered =  tuple[4];
+                $('#docinfo').css('font-size','120%');
+                $("#docinfo").append("<h3>This document was already registered!</h3><h4>Here's some information about it:</h4>")
+                $("#docinfo").append('<p>Author: ' + author + '</p>')
+                $("#docinfo").append('<p>Title: ' + title + '</p>')
+                $("#docinfo").append('<p>Author\'s emailaddress: ' + email + '</p>')
+                $("#docinfo").append('<p>Written on: ' + dateWritten + '</p>')
+                $("#docinfo").append('<p>Registered on: ' + dateRegistered + '</p>')
+
+                $("#docinfo").append('<button type="button" class="btn btn-primary" onclick="window.location=\'/index.html\'">Register Another Document</button>')
+
+              } else {
+                var date = $('#dateWrittenInput').datetimepicker('viewDate')
+                var dateFormatted = parseInt(date.toISOString().slice(0,10).replace(/-/g,""));
+                //register(bytes32 _hash, bytes32 _author,bytes32 _title,bytes32 _email,uint256 _dateWritten
+                var author = web3.toHex($('#author').val());
+                var title = web3.toHex($('#title').val());
+                var email = web3.toHex($('#email').val());
+                var result = docAuthChecker.register(hash, author, title, email , dateFormatted, {from: account});
+              }
+            }
+          )
+        }
+
+        reader.readAsText(file);
+
+        return null;
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+  checkAuthenticity:  function(event)
+  {
     event.preventDefault();
 
     var docAuthChecker;
@@ -73,8 +147,8 @@ App = {
                 var email = web3.toAscii(tuple[2]);
                 var dateWritten = tuple[3];
                 var dateRegistered =  tuple[4];
-$('#docinfo').css('font-size','120%');
-$("#docinfo").append("<h3>This document was already registered! Here's some information about it:</h3>")
+                $('#docinfo').css('font-size','120%');
+                $("#docinfo").append("<h3>This document has been registered!<br/> Here's some information about it:</h3>")
                 $("#docinfo").append('<p>Author: ' + author + '</p>')
                 $("#docinfo").append('<p>Title: ' + title + '</p>')
                 $("#docinfo").append('<p>Author\'s emailaddress: ' + email + '</p>')
@@ -82,13 +156,7 @@ $("#docinfo").append("<h3>This document was already registered! Here's some info
                 $("#docinfo").append('<p>Registered on: ' + dateRegistered + '</p>')
 
               } else {
-                var date = $('#dateWrittenInput').datetimepicker('viewDate')
-                var dateFormatted = parseInt(date.toISOString().slice(0,10).replace(/-/g,""));
-                //register(bytes32 _hash, bytes32 _author,bytes32 _title,bytes32 _email,uint256 _dateWritten
-                var author = web3.toHex($('#author').val());
-                var title = web3.toHex($('#title').val());
-                var email = web3.toHex($('#email').val());
-                var result = docAuthChecker.register(hash, author, title, email , dateFormatted, {from: account});
+                    $("#docinfo").append('This document was not yet registered! <a href="/index.html">Click here to register it.</a>')
               }
             }
           )
