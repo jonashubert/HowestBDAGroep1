3a7d19c1c96d06ed423900ecb3f75b199ea7cab8
Valérie <valerie.siroux@FEBELCO.BE>
2018-04-12 19:54:02 +0200

    poging tot deploycontracts (maar werkt ong niet)

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
-var Doug = artifacts.require("DOUG");
-var DocAuthDB = artifacts.require("DocAuthDB");
-var DocManager = artifacts.require("DocManager");
-var DocAuth = artifacts.require("DocAuth");
+const Doug = artifacts.require("./DOUG.sol");
+const DocAuthDB = artifacts.require("./DocAuthDB.sol");
+const DocManager = artifacts.require("./DocManager.sol");
+const DocAuth = artifacts.require("./DocAuth.sol");
 
 
 
@@ -15,32 +15,33 @@ var DocAuth = artifacts.require("DocAuth");
 });
 }*/
 
-deployer.then(function() {
-  //Create a new version of DOUG
-  return Doug.new();
-}).then(function(instance) {
-  DOUGinst = instance;
-  // Get the deployed instance of DocAuth
-  return DocAuth.deployed();
-}).then(function(instance){
-  DocAuthinst = instance;
-  // Add DocAuth contract to DOUG
-  return DOUGinst.addContract("docauth", DocAuthinst.address);
-}).then(function(instance) {
-  // Get the deployed instance of DocAuthDB
-    return DocAuthDB.deployed;
-}).then(function(instance) {
-    DocAuthDBinst = instance;
-    return DOUGinst.addContract("docauthdb", DocAuthDBinst.address);
-}).then(function(){
-    // Get the deployed instance of DocManager
-    return DocManager.deployed;
-}).then(function(instance){
-  // Add DocManager to doug contract
-    DocManagerinst = instance;
-    return Douginst.addContract("docmanager", DocManagerinst.address);
+module.exports = function(deployer){
+    deployer.then(function() {
+    //Create a new version of DOUG
+    return Doug.new();
+  }).then(function(instance) {
+    DOUGinst = instance;
+    // Get the deployed instance of DocAuth
+    return DocAuth.deployed();
+  }).then(function(instance){
+    DocAuthinst = instance;
+    // Add DocAuth contract to DOUG
+    return DOUGinst.addContract("docauth", DocAuthinst.address);
+  }).then(function(instance) {
+    // Get the deployed instance of DocAuthDB
+      return DocAuthDB.deployed;
+  }).then(function(instance) {
+      DocAuthDBinst = instance;
+      return DOUGinst.addContract("docauthdb", DocAuthDBinst.address);
+  }).then(function(){
+      // Get the deployed instance of DocManager
+      return DocManager.deployed;
+  }).then(function(instance){
+    // Add DocManager to doug contract
+      DocManagerinst = instance;
+      return Douginst.addContract("docmanager", DocManagerinst.address);
 
-})
-
+  })
+}
 
 //http://truffleframework.com/docs/getting_started/migrations
