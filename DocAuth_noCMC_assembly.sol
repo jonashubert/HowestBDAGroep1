950b3903a3cc1d9d98e23611915eba8043549594
Thomas Wielfaert <thomas@wielfaert.be>
2018-04-24 00:47:20 +0200

    rewrote getDocumentByAuthorName(), does not work

--- DocAuth_noCMC_assembly.sol
+++ DocAuth_noCMC_assembly.sol
@@ -1,43 +1,27 @@
-pragma solidity ^0.4.21;
+// MWE for the assembly alternative to the getDocumentByAuthorName function.
+// Code compiles, but does not generate a result due to an intractable bug that remains in the assembly code
 
 contract DocAuth {
-   //This is where we keep all the contracts
+
     mapping(bytes32 => Document) private documentLibrary;
     bytes32[] hashArray;
+    address addr = this;
 
-    /// Function gets an entry from the document library. Returns the metadata of the document.
-    /// Will always return a Document object, even when the document doesn't exist.
-    /// Check the property "isInitialized" to confirm it's not an empty value.
-    function getDocument(bytes32 _hash) public view returns (bytes32,bytes32,bytes32,uint256,uint256, bool) {
-        //todo: teruggeven van struct is blijkbaar niet goed. Hervormen naar "losse variabelen".
-        //https://ethereum.stackexchange.com/questions/3609/returning-a-struct-and-reading-via-web3/3614#3614
-        Document storage _document = documentLibrary[_hash];
-        return (_document.author, _document.title, _document.email, _document.dateWritten, _document.dateRegistered, _document.isInitialized);
-    }
-    
-    function getAuthorOfDoc(bytes32 _hash) public view returns (bytes32) {
-        bytes32 _author;
-        (_author, , , , , ) = getDocument(_hash);
-        return _author;
-    }
+    function register(bytes32 _hash, bytes32 _author,bytes32 _title,bytes32 _email,uint256 _dateWritten) public returns (bool) {
+        if (!isDocumentRegistered(_hash) ) {
+            Document storage _document = documentLibrary[_hash];
+            _document.author = _author;
+            _document.title = _title;
+            _document.email = _email;
+            _document.dateWritten = _dateWritten;
+            _document.dateRegistered = block.timestamp;
+            _document.isInitialized = true;
 
-    function register(bytes32 _hash, bytes32 _author,bytes32 _title,bytes32 _email,uint256 _dateWritten) public returns (bool)
-    {
-       if(!isDocumentRegistered(_hash))
-       {
-           Document storage _document = documentLibrary[_hash];
-           _document.author = _author;
-           _document.title = _title;
-           _document.email = _email;
-           _document.dateWritten = _dateWritten;
-           _document.dateRegistered = block.timestamp;
-           _document.isInitialized = true;
-           
-           hashArray.push(_hash);
-
-           return true;
-       }
-       else { return false; }
+			hashArray.push(_hash);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     function isDocumentRegistered(bytes32 _hash) public view returns (bool) {
@@ -45,34 +29,66 @@ contract DocAuth {
         (, , , , , _storedIsInitialized) = getDocument(_hash);
         return _storedIsInitialized;
     }
-    
-    function findDocument(bytes32 _input) public view returns (bool out) {
-        uint arrayLength = hashArray.length;
-        bytes4 sig = bytes4(keccak256("getAuthorOfDoc(uint256)"));
-        DocAuth dc = this;
-        assembly {
-            let i:= 0
-            loop:
+
+    function getAuthorOfDoc(bytes32 _hash) public view returns (bytes32) {
+	    bytes32 _author;
+	    (_author, , , , , ) = getDocument(_hash);
+	    return _author;
+   }
+
+   function getDocument(bytes32 _hash) public view returns (bytes32,bytes32,bytes32,uint256,uint256, bool) {
+        //todo: teruggeven van struct is blijkbaar niet goed. Hervormen naar tuple.
+        //https://ethereum.stackexchange.com/questions/3609/returning-a-struct-and-reading-via-web3/3614#3614
+        Document storage _document = documentLibrary[_hash];
+        return (_document.author, _document.title, _document.email, _document.dateWritten, _document.dateRegistered, _document.isInitialized);
+    }
+
+   function getDocumentByAuthorNameAsm(bytes32 _author) public returns (bytes32[5] title,uint256[5] dateWritten, uint256[5] dateRegistered, bool[5] isInitialized, bytes32 author, bytes32 email) {
+       bytes32[] memory hashArrayIntern = hashArray;
+       bytes32[] memory resArray;
+       uint arrayLength = hashArray.length;
+
+       bytes4 sig = bytes4(keccak256("getAuthorOfDoc(bytes32)"));
+
+	   assembly {
             let ptr := mload(0x40)
             mstore(ptr,sig)
-            mstore(add(ptr,0x04), mload(add(add(hashArray_slot, 0x20), mul(i, 0x20))))
+            let resArray := msize() // Get the highest available block of memory
+            mstore(add(resArray, 0x00), 5) //set size to 5
+
+            let i:= 0
+            loop:
+            mstore(add(ptr, 0x04), add(hashArrayIntern, add(0x20, mul(i, 0x20))))
             let result := call(
               15000, // gas limit
-              sload(dc),  // to addr. append var to _slot to access storage variable
+              sload(addr_slot),  // to addr. append var to _slot to access storage variable
               0, // not transfer any ether
               ptr, // Inputs are stored at location ptr
-              0x20, // Inputs are 32 bytes long
+              0x24, // Inputs are 36 bytes long
               ptr,  //Store output over input
               0x20) //Outputs are 32 bytes long
-            if eq(_input, result) {
-                out := "true"
-            }
+
+              if eq(mload(ptr), _author) {
+              	mstore(add(resArray, add(0x20, mul(i, 0x20))), add(ptr, 0x20)) // if match, add the document hash to resArray
+              }
+
             i := add(i, 1)
             jumpi (loop, lt(i, arrayLength))
+
+            mstore(0x40, add(resArray, mul(add(i, 1), 0x20))) // update the offset
+        } //end assembly
+
+
+		// Retrieve the found docs from the mappings
+		for (uint i = 0; i < 5; i++)    {
+            (, title[i], , dateWritten[i], dateRegistered[i], isInitialized[i]) = getDocument(resArray[i]);
         }
-    }
-    
-    struct Document {
+        (author, , , , ,) = getDocument(resArray[0]);
+        (, , email, , ,) = getDocument(resArray[0]);
+        return (title, dateWritten, dateRegistered, isInitialized, author, email);
+	    }
+
+	    struct Document {
         bytes32  author;
         bytes32  title;
         bytes32  email;
@@ -80,5 +96,4 @@ contract DocAuth {
         uint256  dateRegistered;
         bool  isInitialized;
     }
-
- }
+}
